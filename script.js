bed255ab6c502280bcef2c34c28ad2a961f65e75
Key Feula <keyfeula1214@gmail.com>
2025-07-28 21:35:52 -0700

    Write html and css for layout of page, need to add button control panel later

---
+++ script.js
@@ -0,0 +1,11 @@
+const playerOneLP = document.querySelector(".player1 .lifepoints");
+const playerTwoLP = document.querySelector(".player2 .lifepoints");
+const p1Add = document.querySelector(".player1 .add");
+const p2Add = document.querySelector(".player2 .add");
+const p1Sub = document.querySelector(".player1 .subtract");
+const p2Sub = document.querySelector(".player2 .subtract");
+const display = document.querySelector(".display");
+
+display.addEventListener("click", (event) => {
+    
+});
